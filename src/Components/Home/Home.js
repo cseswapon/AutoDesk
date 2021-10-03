2d8c644c2d063f5190a3018aba541dc9f9ae6294
cseswapon <cseswaponsaha@gmail.com>
2021-10-04 03:16:43 +0600

    home cover set

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Home.css';
 import cover from '../../img/cover.jpg'
+import logo from '../../img/logo.png'
 const Home = () => {
     return (
-        <div className="add-img">
+        <div>
             <div className="img-cover">
                 <img src={cover} alt="" />
+            </div>
+            <div className="img-logo">
+                <img src={logo} alt="" />
             </div>
         </div>
     );
