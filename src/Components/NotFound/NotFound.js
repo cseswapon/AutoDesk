d0e558f8c935c2ec4957478c99a16668e06e96ef
cseswapon <cseswaponsaha@gmail.com>
2021-10-04 18:14:26 +0600

    notfound file comment

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,6 +3,7 @@ import notfound from '../../img/Monster 404 Error-cuate.png';
 import './NotFound.css';
 const NotFound = () => {
     return (
+        // not fond page image use
         <div className="notfound">
             <img src={notfound} alt="Not found" />
         </div>
